e3027243b32e71df5a1182489eeb5fc6cdb04dac
joachim <joachim.espin@studentfr.ch>
2024-11-03 02:01:58 +0100

    Reset

--- constants/defaultValues.js
+++
@@ -1,7 +0,0 @@
-export const defaultValues = [
-    {name: 'Clicker', image: './asset/clicker_white.png', cost: 10, increase: 1},
-    {name: 'Pickaxe', image: './asset/pickaxe.png', cost: 60, increase: 4},
-    {name: 'Miner', image: './asset/miner.png', cost: 380, increase: 16},
-    {name: 'Factory', image: './asset/factory.png', cost: 3105, increase: 82},
-    {name: 'Potion', image: './asset/potion.png', cost: 23105, increase: 422},
-]
